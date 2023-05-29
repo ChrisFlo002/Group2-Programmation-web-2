c3a02304227477378261b6bfc16252378ffffab7
ChrisFlo002 <floriachristie2019@gmail.com>
2023-05-29 16:47:52 -0700

    Page de produit actualisee

--- produit.php
+++ produit.php
@@ -6,7 +6,38 @@
 <?php
 include("menuEucalyptus.php");
 ?>
-
+<!--========= block pour la présentation de l'atelier=========-->
+<div align="center">
+            <img src="images/atelier de ménuiserie.PNG" id="atelier"> <br>
+            <p>Notre atélier est l'un des meilleurs en matières des technologies utilisées.<br>
+                 Nous sommes convaincu qu'un meilleur environnement de travail favorise une meilleure productiivité,<br>
+                 et donc, une satisfaction garantie.</p>
+        </div>
+        <!--============ block pour la presentation des differents meubles========-->
+        <div align="center">
+            <table>
+                <br>
+                <tr class="present"><!-- ==présentation des tables de bureau=======-->
+                     <img src="images/meuble de bureau.PNG" style="width:200px; height:200px;">
+                     <img src="images/meuble de bureau2.PNG" style="width:200px; height:200px;">
+                    <img src="images/meuble de bureau3.PNG" style="width:200px; height:200px;">
+                    <p> Pour vos projets de construction de meubles de bureau,<br>
+                    nous sommes à votre disposition afin de mettre à l'oeuvre nos talents et expertise pour vous servir</p>
+                </tr><br>
+                <tr class="present"> <!--====présentation des bibliothèques de bureau====-->
+                    <img src="images/bureau bibliothèque.PNG" style="width:300px; height:200px;">
+                    <img src="images/bureau bibliothèque2.PNG" style="width:300px; height:200px;">
+                    <p>Nous sommes aussi qualifiés pour produire des meubles de bibliothèque<br>
+                    personnels ou pour des entreprises.<br>
+                    Nous sommes à l'aise avec les meubles de toutes les tailles.</p>
+                </tr><br>
+                <tr class="present"> <!--=====présentation des meubles de cuisine=====-->
+                    <img src="images/meuble de cuisine bas.PNG" style="width:300px; height:200px;">
+                    <p>Pour vos projets d'amenagements de meubles dans vos ménages,<br>
+                    nous sommes aussi là pour vous servir</p>
+                </tr>
+            </table>
+        </div>
 
 
 <?php
